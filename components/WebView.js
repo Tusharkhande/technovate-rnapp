770acf295bc056f463a2404d1155377b3641a6d8
Khande Tushar Bhanudas <khandetushar2001@gmail.com>
2023-06-14 23:36:48 +0530

    Added functionality to connect to a Wi-Fi network using the eact-native-android-wifi package. Firstly the wifi-qr-code is scanned and the wifi details are extracted from it. Then the indAndConnect function is called with the SSID and password extracted from qr. If the connection is successful, the default gateway IP address is retrieved using the NetworkInfo module and displayed in the console. The setIpAddress function is called to update the state with the default gateway IP address. Finally, a success message is displayed using an Alert component and the default login page of the wifi router is loaded in the webview.

--- components/WebView.js
+++ components/WebView.js
@@ -56,3 +56,8 @@ const styles = StyleSheet.create({
         borderRadius: 20,
       },
 });
+
+feat: Add Wi-Fi connection functionality
+
+Added functionality to connect to a Wi-Fi network using the `react-native-android-wifi` package. Firstly the wifi-qr-code is scanned and the wifi details are extracted from it. Then the `findAndConnect` function is called with the SSID and password extracted from qr. If the connection is successful, the default gateway IP address is retrieved using the `NetworkInfo` module and displayed in the console. The `setIpAddress` function is called to update the state with the default gateway IP address. Finally, a success message is displayed using an `Alert` component and the default login page of the wifi router is loaded in the webview.
+git commit -m "Added functionality to connect to a Wi-Fi network using the `react-native-android-wifi` package. Firstly the wifi-qr-code is scanned and the wifi details are extracted from it. Then the `findAndConnect` function is called with the SSID and password extracted from qr. If the connection is successful, the default gateway IP address is retrieved using the `NetworkInfo` module and displayed in the console. The `setIpAddress` function is called to update the state with the default gateway IP address. Finally, a success message is displayed using an `Alert` component and the default login page of the wifi router is loaded in the webview."
